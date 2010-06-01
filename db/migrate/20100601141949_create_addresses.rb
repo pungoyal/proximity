8d4cdf1a5da2af675d312ae9e1f0e4dc8d362d11
Puneet Goyal <pgoyal@firefly.local>
2010-06-01 19:53:35 +0530

    adding address

---
+++ db/migrate/20100601141949_create_addresses.rb
@@ -0,0 +1,18 @@
+class CreateAddresses < ActiveRecord::Migration
+  def self.up
+    create_table :addresses do |t|
+      t.string :line1
+      t.string :line2
+      t.string :area
+      t.string :city
+      t.string :state
+      t.int :postcode
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :addresses
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100601140551) do
+ActiveRecord::Schema.define(:version => 20100601141949) do
+
+  create_table "addresses", :force => true do |t|
+    t.string   "line1"
+    t.string   "line2"
+    t.string   "area"
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", :force => true do |t|
     t.string   "name"
