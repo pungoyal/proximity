afe940cd96916012a19670510c850563dcd99e45
Puneet Goyal <pungoyal@gmail.com>
2010-06-02 22:39:59 +0530

    tuning the map on screen

--- public/javascripts/proximity.js
+++ public/javascripts/proximity.js
@@ -1,9 +1,13 @@
 window.onload = function() {
-    var latlng = new google.maps.LatLng(5.8, 1.0);
+    var latlong = new google.maps.LatLng(5.8, 1.0);
     var options = {
         zoom: 6,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        center: latlong,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        navigationControlOptions: {
+            style: google.maps.NavigationControlStyle.DEFAULT
+        }
     };
     var map = new google.maps.Map(document.getElementById('map'), options);
 }
