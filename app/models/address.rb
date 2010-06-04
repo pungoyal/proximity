a6b413c699b1a51d4245e2987a22cd3f0b9358fb
Puneet Goyal <pungoyal@gmail.com>
2010-06-04 19:09:32 +0530

    to_s on address

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ActiveRecord::Base
   belongs_to :person
 
   def to_s
-    return line1 + ", " + line2 + ", " + area + ", " + city + ", " + state + ", " + postcode 
+    return (line1.to_s + ", " + line2.to_s + ", " + area.to_s + ", " + city.to_s + ", " + state.to_s + ", " + postcode.to_s).gsub(/ ,/, '')
   end
 end
