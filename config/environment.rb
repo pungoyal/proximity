16064f1b6230804c3f6dbde712d98baa80828a32
Puneet Goyal <pungoyal@gmail.com>
2010-06-05 08:23:08 +0530

    gem manifest

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "sqlite3-ruby", :lib => "sqlite3"
+  config.gem "libxml-ruby"
   config.gem "pg"
   config.gem "haml", :version => "=2.2.14"
   config.gem "wrest"
