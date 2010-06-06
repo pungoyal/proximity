1ab09c454fd560f46dd7eac6a38b0b6013dc91be
Puneet Goyal <pungoyal@gmail.com>
2010-06-06 13:30:24 +0530

    showing a marker on the map

--- public/javascripts/proximity.js
+++ public/javascripts/proximity.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
     var options = {
-        zoom: 6,
-        center: new google.maps.LatLng(5.8, 1.0),
+        zoom: 13,
+        center: new google.maps.LatLng(12.95945073019842, 77.64325065040589),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         mapTypeControl: false
     };
     var map = new google.maps.Map(document.getElementById('map'), options);
+
+    var marker = new google.maps.Marker({
+        map: map,
+        title: 'Marker #',
+        position: new google.maps.LatLng(12.971606, 77.594376),
+        draggable: true
+    });
+    marker.setVisible(true);
 });                                           
